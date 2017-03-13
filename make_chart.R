868a6185c89ad1e99a8545d9c81e646c5264836b
Joe Brew <joebrew@gmail.com>
2017-03-13 18:41:16 +0200

    update for rosauro

--- make_chart.R
+++ make_chart.R
@@ -38,20 +38,68 @@ df <- df %>%
 df <- df %>%
   filter(!is.na(p))
 
+# Filter down
+df <- df %>%
+  filter(key %in% c('HGB', 'Parasitemia', 'WBC'))
+
+# # Rename wbc
+# df$key <-
+#   ifelse(df$key == 'WBC',
+#          'WBCC (leucocytes/microlitre)',
+#          # expression(paste0("WBCC (leucocytes/)", mu, 'L')),
+#          df$key)
+# 
+# # Rename parasitemia
+# df <- df %>%
+#   mutate(key = ifelse(key == 'Parasitemia', 'Parasitemia (parasites/microlitre)',
+#                       ifelse(key == 'HGB', 'HGB (g/dL)',
+#                              key)))
+
+
+
 # plot
 cols <- colorRampPalette(brewer.pal(n = 9, name = 'Set1'))(length(unique(df$key)))
-g <- ggplot(data = df,
+
+# Make a sub dataframe for labeling hgb cut points
+hgb <- df %>% filter(grepl('HGB', key))
+hgb$time <- as.numeric(gsub('h', '', hgb$time))
+# need to interpolate the value at the unknown hours
+left <- data_frame(time = seq(0, max(hgb$time), by = 1))
+hgb <- left_join(left, hgb)
+hgb$p <- zoo::na.approx(object = hgb$p,
+                            x = hgb$time)
+hgb$value <- zoo::na.approx(object = hgb$value,
+                        x = hgb$time)
+hgb <- hgb %>% filter(time %in% c(6, 98, 174))
+hgb$hours <- hgb$time
+hgb$time <- paste0(hgb$time, 'h')
+
+# Get better labels
+
+greeks <- list(
+  bquote('HGB (g/dL)'),
+  bquote(.('Parasitemia (parasites /') ~ mu*.('L)')),
+  # bquote(.('joe'),mu),
+  bquote(.('WBCC (Leococytes /') ~ mu*.('L')))
+
+g <- ggplot() +
+  geom_line(data = df,
             aes(x = hours,
                 y = p,
                 group = key,
                 color = key)) +
-  geom_line() +
-  labs(x = 'Hours after recruitment',
-       y = 'Percentage',
-       title = 'Laboratory values over time',
-       subtitle = 'As percentage of value upon recruitment') +
-  scale_color_manual(name = '',
-                     values = cols) +
-  geom_hline(yintercept = c(0, 100), alpha = 0.6) +
-  scale_y_sqrt(breaks = c(0, 10, 25, 50, 100, 200, 500, 1000, 2000, 3000))
+  labs(x = 'Time after recruitment (hours)',
+       y = 'Percentage (%)') +
+  scale_colour_manual(name = '',
+                      values=cols, 
+                      labels=greeks) +
+  geom_point(data = hgb,
+             aes(x = hours,
+                 y = p),
+             pch = 6,
+             size = 2,
+             col = cols[1]) #+
+  # scale_y_sqrt(breaks = c(0, 10, 25, 50, 100, 200, 500, 1000, 2000, 3000)) 
+  g
 plotly::ggplotly(g)
+
